4cf31c71673fc4ea0abb7d3153aea5f834e08f65
Tairo Moriyama <tairosan@gmail.com>
2013-01-08 18:41:43 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wildcard::Application.config.secret_token = 'dbf47a7723c9266dea657dcc284766b9db10dd24288d16be93f0753ebed2f100bb1b55e95c3bf13e0ec752f1dc23da16f6ce29d6bce2aa8d1edd130d5789c38e'
